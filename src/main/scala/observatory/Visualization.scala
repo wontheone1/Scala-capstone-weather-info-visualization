6c00140b9613f4dd3468dfccf61096c72efe0091
WON SEOB SEO <won.seob.seo@zalando.fi>
2017-10-03 17:13:56 +0300

    Implements linear interpolation and image rendering

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -58,7 +58,30 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+
+    def interpolateColor2(sortedPoints: Array[(Double, Color)], value: Double): Color = {
+      for (i <- 0 until sortedPoints.length - 1) {
+        (sortedPoints(i), sortedPoints(i + 1)) match {
+          case ((v1, Color(r1, g1, b1)), (v2, Color(r2, g2, b2))) => {
+            if (v1 > value)
+              return Color(r1, g1, b1)
+            else if (v2 > value) {
+              val ratio = (value - v1) / (v2 - v1)
+              return Color(
+                math.round(r1 + (r2 - r1) * ratio).toInt,
+                math.round(g1 + (g2 - g1) * ratio).toInt,
+                math.round(b1 + (b2 - b1) * ratio).toInt
+              )
+            }
+          }
+        }
+      }
+      // Value is not within the colourmap.  Return maximum color
+      sortedPoints(sortedPoints.length - 1)._2
+    }
+
+    val sortedPoints = points.toList.sortWith(_._1 < _._1).toArray
+    interpolateColor2(sortedPoints, value)
   }
 
   /**
@@ -67,8 +90,34 @@ object Visualization {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
-    ???
+    val colorMap = colors.toList.sortWith(_._1 < _._1)
+
+    def colorToPixel(c: Color): Pixel = {
+      Pixel(c.red, c.green, c.blue, 255)
+    }
+
+    val buffer = new Array[Pixel](360 * 180)
+
+    for (y <- 0 until 180) {
+      for (x <- 0 until 360) {
+        val temp = inverseDistanceWeighting(temperatures, Location(90 - y, x - 180), P)
+        buffer(y * 360 + x) = colorToPixel(interpolateColor(colorMap, temp))
+      }
+    }
+
+    Image(360, 180, buffer)
   }
+
+  val colours: List[(Double, Color)] = List(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
 
 }
 
