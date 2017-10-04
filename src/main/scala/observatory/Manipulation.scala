15452a301b0fa4fe49db94d04cc5d56ee13aebc6
WON SEOB SEO <won.seob.seo@zalando.fi>
2017-10-04 23:41:51 +0300

    Implement Grid, makeGrid, average, deviation

--- src/main/scala/observatory/Manipulation.scala
+++ src/main/scala/observatory/Manipulation.scala
@@ -11,7 +11,14 @@ object Manipulation {
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Double)]): (Int, Int) => Double = {
-    ???
+    val grid: Map[Location, Double] = {
+      for {
+        lat <- -89 to 90
+        lon <- -180 to 179
+      } yield Location(lat, lon) -> Visualization.predictTemperature(temperatures, Location(lat, lon))
+    }.toMap
+
+    (lat, lon) => grid(Location(lat, lon))
   }
 
   /**
@@ -20,7 +27,16 @@ object Manipulation {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Double)]]): (Int, Int) => Double = {
-    ???
+    val grids: Iterable[(Int, Int) => Double] = temperaturess.map(makeGrid)
+
+    val grid: Map[Location, Double] = {
+      for {
+        lat <- -89 to 90
+        lon <- -180 to 179
+      } yield Location(lat, lon) -> grids.map(grid => grid(lat, lon)).sum / grids.size
+    }.toMap
+
+    (lat, lon) => grid(Location(lat, lon))
   }
 
   /**
@@ -29,7 +45,8 @@ object Manipulation {
     * @return A grid containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Double)], normals: (Int, Int) => Double): (Int, Int) => Double = {
-    ???
+    val grid = makeGrid(temperatures)
+    (lat, lon) => grid(lat, lon) - normals(lat, lon)
   }
 
 
